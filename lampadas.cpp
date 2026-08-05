f04637b226a74f940a7475747356b5fa897eb1ad
potatoADSE <matiasaciole2009@gmail.com>
2026-08-05 02:06:50 -0300

    refactor: usa int com estados numericos para AisOn e BisOn
    Substitui bool por int (-3, 3) para rastrear estado das lampadas. I1 alterna AisOn entre os valores -3/3, I2 inverte o sinal de AisOn e BisOn.

--- lampadas.cpp
+++ lampadas.cpp
@@ -4,26 +4,27 @@ using namespace std;
 
 int main() {
 	int N;
-	bool AisOn = false;
-	bool BisOn = false;
+	int AisOn = -3;
+	int BisOn = -3;
 	cin >> N;
 	vector <int> intrp(N);
 
 	for (int i = 0; i < N; i++) {
 		cin >> intrp[i];
-		if (intrp[i] == 1) {
-			AisOn = true;
+		if (intrp[i] == 1 && AisOn == 2) {
+			AisOn = 3;
+		}
+		else if (intrp[i] == 1 && AisOn == 3) {
+			AisOn = -3;
 		}
 		else if (intrp[i] == 2) {
-			bool hand;
-			hand = BisOn;
-			BisOn = AisOn;
-			AisOn = hand;
+			BisOn *= -1;
+			AisOn *= -1;
 		}
 	}
 
-	AisOn == true ? cout << 1 << endl : cout << 0 << endl;
-	BisOn == true ? cout << 1 << endl : cout << 0 << endl;
+	AisOn == 3 ? cout << 1 << endl : cout << 0 << endl;
+	BisOn == 3 ? cout << 1 << endl : cout << 0 << endl;
 
 	return 0;
 }
