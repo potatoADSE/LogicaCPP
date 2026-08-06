776c752a14656f6dcad396327316b5995aefb376
potatoADSE <matiasaciole2009@gmail.com>
2026-08-06 00:34:32 -0300

    Resolve erro de logica: comsecutivos.cpp
    Utiliza streak = 1, ao invés de 0; troca storage.push_back(streak) para storage.insert(storage.begin() + storIndice, numeros[i]) que faz o armazenamento da variavel streak no array storage, na posição certa

--- consecutivos.cpp
+++ consecutivos.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	int N;
-	int streak = 0;
+	int streak = 1;
 	cin >> N;
 	vector <int> numeros(N);
 	vector <int> storage;
@@ -13,13 +13,13 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		cin >> numeros[i];
 	}
-	for (int i = 0; i < N-1; i++) {
+	for (int i = 0; i < N - 1; i++) {
 		if (numeros[i] == numeros[i + 1]) {
 			streak++;
-			storage.push_back(streak);
+			storage.insert(storage.begin() + storIndice, streak);
 		}
 		else if (numeros[i] != numeros[i + 1]) {
-			streak = 0;
+			streak = 1;
 			storIndice++;
 		}
 	}
